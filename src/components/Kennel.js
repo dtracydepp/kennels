4598490dde0d7bac667b6bce421586534adfa7eb
dtracydepp <tracydepp82@gmail.com>
2020-11-23 15:55:35 -0600

    added LocationProvider and LocationList components, refactored Location component, and refactored Kennel component, removed hardcoded components to use live data

--- src/components/Kennel.js
+++ src/components/Kennel.js
@@ -1,49 +1,16 @@
 import React from "react"
-import { Animal } from "./animal/Animal"
-import {Location} from "./location/Location"
-import {Employee} from "./employee/Employee"
-import {Customer} from "./customer/Customer"
-import "./animal/Animal.css"
-import "./location/Location.css"
-import "./employee/Employee.css"
-import "./customer/Customer.css"
+import { LocationList } from "./location/LocationList"
+import { LocationProvider } from "./location/LocationProvider"
+import "./Kennel.css"
 
 export const Kennel = () => (
     <>
         <h2>Nashville Kennels</h2>
         <small>Loving care when you're not there.</small>
 
-        <address>
-            <div>Visit Us at the Nashville North Location</div>
-            <div>500 Puppy Way</div>
-        </address>
-
-        <h2>Animals</h2>
-        <article className="animals">
-            <Animal />
-            <Animal />
-            <Animal />
-        </article>
-
-        <h2>Employees</h2>
-        <article className="employees">
-            <Employee />
-            <Employee />
-            <Employee />
-        </article>
         <h2>Locations</h2>
-        <article className="locations">
-            <Location />
-            <Location />
-        </article>
-
-        <h2>Customers</h2>
-        <article className="customers">
-            <Customer />
-            <Customer />
-            <Customer />
-            <Customer />
-
-        </article>
+        <LocationProvider>
+            <LocationList />
+        </LocationProvider>
     </>
 )
