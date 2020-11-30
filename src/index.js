6327d644d09102d21e361c542ef17a9836b5a471
dtracydepp <tracydepp82@gmail.com>
2020-11-30 15:23:43 -0600

    fixing errors

--- src/index.js
+++ src/index.js
@@ -1,10 +1,13 @@
 import React from "react"
 import ReactDOM from "react-dom"
+import {BrowserRouter as Router} from "react-router-dom"
 import { Kennel } from "./components/Kennel"
 
 ReactDOM.render(
   <React.StrictMode>
+    <Router>
     <Kennel />
+    </Router>
   </React.StrictMode>,
   document.getElementById('root')
 );
