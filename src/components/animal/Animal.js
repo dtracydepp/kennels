871b70e5e7ee46134ef506b0b57654f935ec9406
dtracydepp <tracydepp82@gmail.com>
2020-12-02 12:19:31 -0600

    enabled login form and registration form, refactored kennel component

--- src/components/animal/Animal.js
+++ src/components/animal/Animal.js
@@ -6,6 +6,6 @@ export const Animal = ({ animal, customer, location }) => (
         <h3 className="animal__name">{animal.name}</h3>
         <div className="animal__breed">Breed: {animal.breed}</div>
         <div className="animal__location">Location: {location.name}</div>
-        <div className="animal__owner">Customer: {customer.name}</div>
+        {/* <div className="animal__owner">Customer: {customer.name}</div> */}
     </section>
 )
